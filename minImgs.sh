29551d87ed60e966df10c095c4079c76655ba1d3
sonickseven <sonickfaber7@yahoo.es>
2018-06-07 09:11:04 -0500

    aergsergser

--- minImgs.sh
+++ minImgs.sh
@@ -3,8 +3,7 @@ maxWidth=250
 for img in *.jpg ; do
 	widthImg=$(identify -format %w "$img")
 	if [ "$widthImg" -gt "$maxWidth" ] ; then
+		echo "cambiar $img"
 		$(convert $img -resize "250x250!" -quality 76 $img)
-	else
-		echo 'no se hace nada'
 	fi
 done
